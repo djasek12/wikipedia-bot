d3d32f1de9e8aca5f222f213a743acc3323294ae
Eric Krebs <ekrebs@nd.edu>
2016-12-04 17:02:52 -0500

    added to main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import wikipedia as wiki
 import webutils
 
 
-
-
-
-
-
 def traverse(src, dst):
 	frontier = [] # heap containing the next locations to go to
 	marked = set() # contains all visited links
 
 	for links in links:
 		# heap.heappush(Jaccard(dst, link), link)
+
+
+
+
+
 
 	
 		
