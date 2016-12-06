ca2232ffdf2cc3a021cffd752997df2839b9e69e
Daniel Jasek <djasek@nd.edu>
2016-12-06 10:23:23 -0500

    update formatting

--- wikipedia_bench.py
+++ wikipedia_bench.py
@@ -2,10 +2,14 @@ import time
 import subprocess
 
 src_dst_list = []
+src_dst_list.append(['chicago cubs', 'wrigley field'])
+src_dst_list.append(['university of notre dame', 'knute rockne'])
 src_dst_list.append(['chocolate', 'brain'])
 src_dst_list.append(['orange (fruit)', 'purple'])
+src_dst_list.append(['angela merkel', 'peyton manning'])
 
-print "| Source | Destination | # Link Separation | Elapsed Time "
+print "| Source                         | Destination                    | # Link Separation | Elapsed Time "
+print "-----------------------------------------------------------------------------------------------------"
 
 for pair in src_dst_list:
     command = 'python wikipedia-bot.py "' + pair[0] + '" "' + pair[1] + '"'
@@ -13,4 +17,4 @@ for pair in src_dst_list:
     output = subprocess.check_output(command, shell=True)
     distance = output.split("Distance:")[1].split()[0]
     end = time.time()
-    print "| " + pair[0] + " | " + pair[1] + " | " + distance + " | " + str((end-start)) + ' s'
+    print "| " + pair[0] + ' '*(30-len(pair[0])) +" | " + pair[1] + ' '*(30-len(pair[1])) + " | " + distance + "                 | " + str((end-start)) + ' s'
