a028433fe78340d9c27510ec205f48830341c79c
Daniel Jasek <djasek@nd.edu>
2016-12-07 11:04:09 -0500

    testing if CI will fail

--- test.sh
+++ test.sh
@@ -1,25 +1,44 @@
-timeout1=15
+timeout1=10
 timeout2=30
-timeout3=45
+timeout3=1
 
 
 #test correctness
 echo "Testing Correctness"
 command=`python wikipedia-bot.py "Evansville, Indiana" "Bosse Field" | tail -n 1`
 output="Distance: 1 | Evansville, Indiana -> Bosse Field"
-diff <(echo "$command") <(echo "$output")
+if [[ `diff <(echo "$command") <(echo "$output")` ]] ; then
+    echo "Test failed"
+    exit 1
+fi
 
 command=`python wikipedia-bot.py "University of Notre Dame" "Knute Rockne" | tail -n 1`
-output="Distance: 1 | University of Notre Dame ->Knute Rockne"
+output="Distance: 1 | University of Notre Dame -> Knute Rockne"
 diff <(echo "$command") <(echo "$output")
+if [[ `diff <(echo "$command") <(echo "$output")` ]] ; then
+    echo "Test Failed"
+    exit 1
+fi
 
 
 #test performance
 echo "Testing Articles 1 Link Apart with Timeout of $timeout1 seconds"
 timeout $timeout1 python wikipedia-bot.py "Evansville, Indiana" "Bosse Field" > /dev/null 2>&1
+if [[ $? -eq 124  ]] ; then
+    echo "Test Failed"
+    exit 1
+fi
 
 echo "Testing Articles 2 Links Apart with Timeout of $timeout2 seconds"
 timeout $timeout2 python wikipedia-bot.py "chocolate" "brain" > /dev/null 2>&1
+if [[ $? -eq 124  ]] ; then
+    echo "Test Failed"
+    exit 1
+fi
 
 echo "Testing Articles 3 Links Apart with Timeout of $timeout3 seconds"
 timeout $timeout3 python wikipedia-bot.py "Bob Ross" "red" > /dev/null 2>&1
+if [[ $? -eq 124  ]] ; then
+    echo "Test Failed"
+    exit 1
+fi
